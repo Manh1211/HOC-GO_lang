00ee98b1048e1a4ed4567df88deec091feb224f8
Manh <sirostudy@example.com>
2021-03-20 22:32:32 +0700

    new update

--- main.go
+++ main.go
@@ -2,59 +2,60 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
-//Point is a 2d point
-type Point struct {
-	X int
-	Y int
-}
-
-//Square is a square
-type Square struct {
-	Center Point
-	Length int
-}
-
-//Moved moves the point
-func (p *Point) Moved(dx int, dy int) {
-	p.X += dx
-	p.Y += dy
-
-}
-
-//NewSuqare return a new square
-func NewSuqare(x int, y int, length int) (*Square, error) {
-	if length <= 0 {
-		return nil, fmt.Errorf("Length must be > 0")
-	}
-
-	s := &Square{
-		Center: Point{x, y},
-		Length: length,
-	}
-
-	return s, nil
-}
-
-//S.method have prototype P.method
-func (s *Square) Move(dx int, dy int) {
-	s.Center.Moved(dx, dy) // s --> centrer --> Move
-}
-
-//Area return the square are
-func (s *Square) Area() int {
-	return s.Length * s.Length
-}
-
 func main() {
-	s, err := NewSuqare(1, 1, 10)
-	if err != nil {
-		log.Fatalf("ERROR: can't create square")
+	// var hs1 thongtin = &study{"cntt1","to kim manh",18}
+	// var tc1 thongtin = &teacher{"cntt1","su van luong","giai tich",30}
+	hs1 := study{
+		"cntt1",
+		"to kim manh",
+		18,
 	}
-
-	s.Move(2, 3)
-	fmt.Printf("%+v\n", s)
-	fmt.Println(s.Area())
+	hs1.Show()
 }
+
+type study struct {
+	class string
+	name  string
+	age   int
+}
+
+type teacher struct {
+	class   string
+	name    string
+	subject string
+	age     int
+}
+
+func (hs *study) Show() {
+	fmt.Println(
+		hs.class,
+		hs.name,
+	)
+}
+
+func (tc *teacher) show() {
+	// fmt.Println(
+	// 	tc.class,
+	// 	tc.name,
+	// 	tc.subject,
+	// )
+	tc.name.Show()
+}
+
+// func (hs *study) old() {
+// 	fmt.Println(hs.age)
+// }
+
+// func (tc *teacher) old() {
+// 	fmt.Println(
+// 		"tuoi cua toi la:",
+// 		tc.age,
+// 	)
+// }
+
+// type thongtin interface{
+// 	show()
+// 	old()
+// }
